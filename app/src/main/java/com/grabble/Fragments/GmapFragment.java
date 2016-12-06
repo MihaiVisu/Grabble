a6ff2fe01134867cb75aab37132880b27e99f6a6
Mihai <mihaivisuian@gmail.com>
2016-12-06 20:19:34 +0000

    add radius circles

--- app/src/main/java/com/grabble/Fragments/GmapFragment.java
+++ app/src/main/java/com/grabble/Fragments/GmapFragment.java
@@ -4,21 +4,22 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
+import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationManager;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -26,22 +27,22 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.grabble.R;
 
 
-import static android.content.Context.LOCATION_SERVICE;
-
-
 public class GmapFragment extends Fragment implements
         OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationSource.OnLocationChangedListener {
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private LatLng myPosition;
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private GoogleMap mMap;
+    private Circle grabbingRadius, lineOfSight;
 
     @Nullable
     @Override
@@ -84,25 +85,25 @@ public class GmapFragment extends Fragment implements
         mMap = googleMap;
 
         if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 100);
-            return;
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
         }
         mMap.setMyLocationEnabled(true);
 
-        if (mLastLocation != null) {
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()))
-                    .title("You are here!")
-            );
-        }
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
     private void createLocationRequest() {
+
+        if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+        }
 
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(10000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -113,7 +114,18 @@ public class GmapFragment extends Fragment implements
             requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 100);
         }
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
+        if (mLastLocation!= null && mMap != null) {
+            lineOfSight = mMap.addCircle(new CircleOptions()
+                    .center(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()))
+                    .radius(100)
+                    .strokeColor(Color.TRANSPARENT)
+                    .fillColor(Color.parseColor("#70303F9F")));
+            grabbingRadius = mMap.addCircle(new CircleOptions()
+                    .center(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()))
+                    .radius(50)
+                    .strokeColor(Color.TRANSPARENT)
+                    .fillColor(Color.parseColor("#703F51B5")));
+        }
         createLocationRequest();
     }
 
@@ -129,6 +141,12 @@ public class GmapFragment extends Fragment implements
 
     @Override
     public void onLocationChanged(Location location) {
-
+        if (grabbingRadius != null) {
+            grabbingRadius.setCenter(new LatLng(location.getLatitude(), location.getLongitude()));
+        }
+        if (lineOfSight != null) {
+            lineOfSight.setCenter(new LatLng(location.getLatitude(), location.getLongitude()));
+        }
     }
+
 }
