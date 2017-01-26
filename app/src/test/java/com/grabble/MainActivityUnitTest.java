4a457202615b606ba4d404e8fe548906c5f49ba7
Mihai <mihaivisuian@gmail.com>
2017-01-26 23:12:51 +0000

    config gradle for instrumented tests

---
+++ app/src/test/java/com/grabble/MainActivityUnitTest.java
@@ -0,0 +1,14 @@
+package com.grabble;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MainActivityUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
